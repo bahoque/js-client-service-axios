f32c9c5f5af181f57b140c3680a19432a6eb3cfa
Ezequiel Alfonso Bahoque Lopez <ezequielbahoquelopez@gmail.com>
2021-02-27 10:22:59 -0500

    client tets

--- src/service.ts
+++ src/service.ts
@@ -3,11 +3,63 @@ import { from, Observable } from "rxjs";
 import { Service as CoreService, TService } from "@bahoque/client-service-core";
 
 export class Service<T1 = any> extends CoreService<T1> {
-    constructor() {
-        super();
-    }
+	private _timeout: number;
+	private _url: string;
+	private _path: string;
 
-    raw<T>(method: keyof TService<T>, ...rest: any): Observable<T> {
-        return null
-    }
+	private _client: AxiosInstance;
+
+	constructor(path: string) {
+		super();
+
+		this._path = path;
+
+		this.makeClient();
+	}
+
+	get client(): AxiosInstance {
+		return this._client;
+	}
+
+	set client(value: AxiosInstance) {
+		this._client = value;
+	}
+
+	get path(): string {
+		return this._path;
+	}
+
+	set path(value: string) {
+		this._path = value;
+		this.makeClient();
+	}
+
+	get timeout(): number {
+		return this._timeout;
+	}
+
+	set timeout(value: number) {
+		this._timeout = value;
+		this.makeClient();
+	}
+
+	get url(): string {
+		return this._url;
+	}
+
+	set url(value: string) {
+		this._url = value;
+		this.makeClient();
+	}
+
+	private makeClient(): void {
+		this._client = axios.create({
+			baseURL: this._url + this._path,
+			timeout: this._timeout,
+		});
+	}
+
+	raw<T>(method: keyof TService<T>, ...rest: any): Observable<T> {
+		return null;
+	}
 }
--- test/client.test.js
+++ test/client.test.js
@@ -1,2 +1,21 @@
-// api https://jsonplaceholder.typicode.com/
-describe('create client', () => {});
+const axios = require('axios');
+
+const base = 'https://jsonplaceholder.typicode.com/';
+const timeout = 3000;
+
+describe('create client', () => {
+    const client = axios.create({
+        baseURL: base,
+        timeout
+    });
+    const client2 = axios.create({
+        baseURL: 'https://www.google.com.co',
+        timeout
+    });
+
+    it('inspect client', async function (done) {
+        console.log(client.defaults.baseURL);
+        console.log(client2.defaults.baseURL);
+        done();
+    });
+});
