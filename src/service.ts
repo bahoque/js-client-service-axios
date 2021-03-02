7eda7eef105914aac1e1e8bca29d570e743cf800
Ezequiel Alfonso Bahoque Lopez <ezequielbahoquelopez@gmail.com>
2021-03-01 21:36:21 -0500

    version 1.0.0

--- src/service.ts
+++ src/service.ts
@@ -1,6 +1,20 @@
-import axios, { AxiosInstance } from "axios";
-import { from, observable, Observable } from "rxjs";
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+import { from, Observable } from "rxjs";
 import { Service as CoreService, TService } from "@bahoque/client-service-core";
+
+function getData<T>(p: Promise<AxiosResponse<T>>): Promise<T> {
+	return p.then(it => it.data)
+}
+
+function getId(method: keyof TService<any>, p: any): any {
+	return ["find", "create"].includes(method) ? null : p[0];
+}
+
+function getParams(p: any): string {
+	const query = p[p.length - 1]?.query;
+	if (query && Object.keys(query).length) return `?${new URLSearchParams(query).toString()}`;
+	return "";
+}
 
 export class Service<T1 = any> extends CoreService<T1> {
 	private _client: AxiosInstance;
@@ -46,29 +60,23 @@ export class Service<T1 = any> extends CoreService<T1> {
 		this._client = axios.create({ baseURL, timeout, url });
 	}
 
-	raw<T>(method: keyof TService<T>, ...rest: any): Observable<T> {
-		console.log(method);
+	raw<T>(method: keyof TService<any>, ...p: any): Observable<T> {
+		const id = getId(method, p);
+		const params = getParams(p);
+		const path = `/${this.path}${!!id ? `/${id}` : ""}${params}`;
+		const config = { headers: p[p.length - 1]?.headers };
 		switch (method) {
 			case "get":
-				return new Observable<T>((subscriber) => {
-					console.log(1);
-					this._client
-						.get<T>(`/users/${rest[0]}`)
-						.then((data) => {
-							console.log(2);
-							subscriber.next(data.data);
-							subscriber.complete();
-						})
-						.catch((error) => {
-							console.log(3);
-							subscriber.error(error);
-						});
-				});
-			default:
-				return new Observable<T>((subscriber) => {
-					subscriber.next();
-					subscriber.complete();
-				});
+			case "find":
+				return from(getData(this._client.get<T>(path, config)));
+			case "create":
+				return from(getData(this._client.post<T>(path, p[0], config)));
+			case "update":
+				return from(getData(this._client.put<T>(path, p[1], config)));
+			case "patch":
+				return from(getData(this._client.patch<T>(path, p[1], config)));
+			case "remove":
+				return from(getData(this._client.delete<T>(path, config)));
 		}
 	}
 }
--- test/client.test.js
+++ test/client.test.js
@@ -1,32 +1,33 @@
-const axios = require('axios');
-const service = require('../src/service');
+const axios = require("axios");
+const service = require("../src/service");
 
-const base = 'https://jsonplaceholder.typicode.com';
+const base = "https://jsonplaceholder.typicode.com";
 const timeout = 3000;
 
-describe('create service', () => {
+describe("create service", () => {
+	const user = new service.Service(base, "users", timeout);
 
-    const user = new service.Service(base, 'users', timeout);
+	it("request get", function (done) {
+		user.get(1).subscribe((data) => {
+			done();
+		});
+	});
 
-    it('request get', function (done) {
-        user.get(1).subscribe(data => {
-            done();
-        })
-    });
+	it("request find", function (done) {
+		user.find({ }).subscribe((data) => {
+			done();
+		});
+	});
 
-    it('request find', async function (done) {
-        done();
-    });
+	it("request create", async function (done) {
+		done();
+	});
 
-    it('request create', async function (done) {
-        done();
-    });
+	it("request update", async function (done) {
+		done();
+	});
 
-    it('request update', async function (done) {
-        done();
-    });
-
-    it('request patch', async function (done) {
-        done();
-    });
+	it("request patch", async function (done) {
+		done();
+	});
 });
